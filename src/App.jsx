fb4ddc78f3ad63ad1f95b01b75f2002044d79e39
Yusuf <starbucksandapple@gmail.com>
2024-08-30 23:11:55 +0300

    conditional rendering & render lists

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,23 @@ import Card from './Card.jsx';
 import Button from "./Button/Button.jsx";
 import Student from "./Student.jsx";
 import UserGreeting from "./UserGreeting.jsx";
-
+import List from './List.jsx';
 
 function App() {
-    return(
+
+        const fruits = [{id:1, name:"apple", calories: 95},
+                                                {id:2, name:"banana", calories: 45},
+                                                {id:3, name:"orange", calories: 105},
+                                                {id:4, name:"coconut", calories: 159},
+                                                {id:5, name:"pineapple", calories: 37}];
+
+        const vegetables = [{id:6, name:"potatoes", calories: 110},
+                                                {id:7, name:"calery", calories: 15},
+                                                {id:8, name:"carrots", calories: 25},
+                                                {id:9, name:"corn", calories: 63},
+                                                {id:10, name:"broccoli", calories: 50}];
+
+        return(
         <>
             <Header/>
 
@@ -27,9 +40,13 @@ function App() {
 
             <UserGreeting isLoggedIn={true} username="Josef"/>
 
+                {fruits.length > 0 && <List items={fruits} category="Fruits"/>}
+                {vegetables.length > 0 ? <List items={vegetables} category="Vegetables"/> : null}
+
             <Footer/>
         </>
     );
 }
+
 
 export default App
--- src/UserGreeting.jsx
+++ src/UserGreeting.jsx
@@ -1,26 +1,22 @@
 import PropTypes from "prop-types";
 
-function UserGreeting(props) {
+function UserGreeting({isLoggedIn = false, username = "Guest"}) {
 
     const welcomeMessage = <h2 className="welcome-message">
-                                    Welcome {props.username}
+                                    Welcome {username}
                                     </h2>
 
     const loginPrompt = <h2 className="login-prompt">
                                 Please log in to continue
                                 </h2>
 
-    return (props.isLoggedIn ? welcomeMessage : loginPrompt);
+    return (isLoggedIn ? welcomeMessage : loginPrompt);
 
 }
 
 UserGreeting.propTypes = {
     isLoggedIn: PropTypes.bool,
     username: PropTypes.string,
-}
-UserGreeting.defaultProps = {
-    isLoggedIn: false,
-    username: "Guest",
 }
 
 export default UserGreeting;
